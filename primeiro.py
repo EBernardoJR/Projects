7e485b0726d749ce9ece16fc53623ab087e3c333
Emanoel Bernardo Junior <50339193+EBernardoJR@users.noreply.github.com>
2019-06-15 18:54:43 -0300

    Add files via upload

---
+++ primeiro.py
@@ -0,0 +1,7 @@
+valor = float(input('Digite o valor: '))
+juros = [0.94, 0.93, 0.91, 0.89, 0.88, 0.87, 0.85, 0.83, 0.82, 0.80, 0.79, 0.77]
+print(f'valor a vista {valor}')
+for i, v in enumerate(juros):
+    valortot = valor / v
+    parcela = valortot / (i + 1)
+    print(f'{i + 1}X de {parcela:.2f}. Total: {valortot:.2f}')
